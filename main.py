32548f01dd4400da36187a3ce7b0342babacb350
francisadediran <francis.adediran@publicissapient.com>
2020-04-15 14:48:02 +0100

    installation and resource update

--- main.py
+++ main.py
@@ -28,4 +28,5 @@ def india_metrics():
                     mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='127.0.0.1', port=8080, debug=True)
+   # app.run()
