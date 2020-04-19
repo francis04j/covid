baf76c996c4f7192a443914dba4a14937204510c
francisadediran <francis.adediran@publicissapient.com>
2020-04-19 19:45:42 +0100

    added authentication with jwt

--- app-resource.py
+++ app-resource.py
@@ -1,12 +1,21 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key="francis"
 api = Api(app)
+
+jwt=JWT(app, authenticate, identity)    # new JWT object that creates /auth endpoint
+                                        # auth endpoint returns JWT token that is then used with subsequent requests
+                                        # it uses the identity function to verify the token 
 
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         # return {'item': next(filter(lambda x: x['name'] == name, items), None)}
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -23,7 +32,7 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        return jsonify({'items', items})
+        return {'items': items}
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
